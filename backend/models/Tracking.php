e722dd2041d6e127ebda359068366f51cab37531
chetat <yekuwilfred@gmail.com>
2020-04-29 14:31:13 +0100

    Implement Read trackings

--- backend/models/Tracking.php
+++ backend/models/Tracking.php
@@ -20,7 +20,14 @@ class Tracking {
     // constructor with $db as database connection
     public function __construct($db){
             $this->conn = $db;
-        }
+    }
+
+    public function read(){
+        $query = "SELECT * FROM ". $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
 }
 
 ?>
--- backend/tracking/read.php
+++ backend/tracking/read.php
@@ -19,7 +19,37 @@ $num = $stmt->rowCount();
 
 if ($sum > 0){
     // tracking items array
+    $trackings_array = array();
+    $trackings_array["data"] = array();
+
+    //retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $tracking_item = array(
+            "id" => $id,
+            "content" => $content,
+            "shipped_date" => $shipped_date,
+            "estimated_date" => $estimated_date,
+            "shipment_type" =>$shipment_type,
+            "tracking_number" => $tracking_number,
+            "receiver_name" => $receiver_name,
+            "receiver_address" => $receiver_address,
+            "telephone" =>$telephone,
+            "destination" => $destination,
+            "current_location" => $current_location,
+            "status" => $status
+        );
+        array_push($trackings_array["data"], $tracking_item);
+    }
+
+    http_response_code(200);
+
+    echo json_encode($trackings_array);
     
+}else{
+    echo json_encode([]);
 }
 
 ?>
